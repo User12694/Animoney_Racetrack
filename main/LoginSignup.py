fbe65320c73c4da4c5eb17ffbbd99d7ba240a4da
User12694 <89754537+User12694@users.noreply.github.com>
2023-12-17 15:06:38 +0700

    Update LoginSignup.py

--- main/LoginSignup.py
+++ main/LoginSignup.py
@@ -6,6 +6,7 @@ import smtplib
 import random
 import string
 import os
+from datetime import datetime
 
 login_lock = False
 WINDOW_SIZES = [(1280,720),(768,432)]
@@ -80,6 +81,8 @@ class LoginMenu:
         # self.open_image_button = ctk.CTkButton(self.frame, text="Open image", font=("default", 12*ratio, "bold"), fg_color="transparent", width= 78*ratio, height=20*ratio, command=self.open_image)
         # self.open_image_button.place(x=303*ratio, y= 758*ratio)
 
+        # Tạo nút đăng kí bằng email
+        self.email_register = ctk.CTkButton(self.frame, text="Sign up by email", font=("default",12*ratio,"bold"),fg_color="transparent",width=145*ratio, height=20*ratio,command=self.switch_to_register)
         # Tạo hình ảnh và đặt ở tọa độ (156,71)
         self.image = Image.open("./main/image_transparent.png")
         self.image = self.image.resize((int(200*ratio), int(200*ratio)))
@@ -91,27 +94,22 @@ class LoginMenu:
         self.login_label = ctk.CTkLabel(self.frame, text="Login", font=("default", 32*ratio, "bold"), fg_color="transparent", width=177*ratio, height=45*ratio)
         self.login_label.place(x=167*ratio, y=271*ratio)
 
-        self.confirm_password_label = ctk.CTkLabel(self.frame, text="Password", font=("default", 20*ratio, "bold"))
+        self.confirm_password_label = ctk.CTkLabel(self.frame, text="Confirm Password", font=("default", 20*ratio, "bold"))
+        self.confirm_password_entry = ctk.CTkEntry(self.frame, width=343*ratio, height=54*ratio, fg_color="white", border_width=1, border_color="black", corner_radius=20, font=("default", 20*ratio),show='•')
+        self.confirm_password_entry.configure(text_color="black")
 
-        self.confirm_password_entry = ctk.CTkEntry(self.frame, width=343*ratio, height=54*ratio, fg_color="white", border_width=2, border_color="black", corner_radius=20, font=("default", 20*ratio),show='•')
+        self.back_button = ctk.CTkButton(master=self.frame, text="Back", font=("default",12*ratio,"bold"),fg_color="transparent",width=65*ratio, height=20*ratio,command=self.switch_to_register)
+
     def open_image(self):
         pass
     def draw(self):
         # Lặp vô tận để hiển thị cửa sổ
         self.window.mainloop()
-    def choice_to_register(self):
-        self.new_window = ctk.CTkToplevel(self.window)
-        self.new_window.title("Register")
-        self.new_window.geometry('250x200')
-        self.new_frame = ctk.CTkFrame(self.new_window, width= self.new_window._current_width, height=self.new_window._current_height)
-        self.new_frame.place(x=0, y=0)
-        self.signup_button1 = ctk.CTkButton(self.new_frame, text="Sign up by Google", font=('Montserrat',16*ratio,"bold"), width=100*ratio, height=40*ratio, command=self.switch_to_register)
-        self.signup_button1.place(x = 25*ratio, y = 40*ratio)
-        
-        self.signup_button2 = ctk.CTkButton(self.new_frame, text="Sign up by local account", width=100*ratio, height=40*ratio, command=self.create_account)
-        self.signup_button2.place(x = 25*ratio, y = 100*ratio)
-
-    def switch_to_register(self): # Đăng kí bằng email
+    
+    # Đăng kí bằng email
+    def switch_to_register(self):
+        global ratio
+        self.login_label.configure(text="Register")
         clear_entry(self.username_entry)
         clear_entry(self.password_entry)
         clear_entry(self.confirm_password_entry)
@@ -119,16 +117,20 @@ class LoginMenu:
         self.username_label.configure(text="Email")
         self.password_label.place_forget()
         self.password_entry.place_forget()
-
+        self.confirm_password_entry.place_forget()
+        self.confirm_password_label.place_forget()
+        self.email_register.place_forget()
         # Chuyển đổi text trên nút "Login" thành "Register"
         self.login_button.configure(text="Confirm Email", command= self.confirm_email)
-
         # Thay đổi chữ trong label "Don't have an account?" thành "Already have one?"
         self.account_label.configure(text="Already have one?")
-
         # Thay đổi chữ trên nút "Sign up" thành "Sign in"
         self.signup_button.configure(text="Sign in", command=self.switch_to_login)
+        self.back_button.configure(command=self.create_account)
+        self.back_button.place(x = 233*ratio, y = 788*ratio)
+
     def switch_to_login(self):
+        self.email_register.place_forget()
         self.login_label.configure(text="Login")
         self.username_label.configure(text="Username/Email")
         self.login_button.configure(text="Login")
@@ -137,6 +139,7 @@ class LoginMenu:
         self.login_button.place_forget()
         self.password_label.place_forget()
         self.password_entry.place_forget()
+        self.back_button.place_forget()
         clear_entry(self.username_entry)
         clear_entry(self.password_entry)
         clear_entry(self.confirm_password_entry)
@@ -155,31 +158,35 @@ class LoginMenu:
         else:
             if check_gmail_in_string(email_input) == True:
                 code = send_verification_code(email_input)
+                clear_entry(self.username_entry)
                 email = email_input
                 self.login_button.configure(text="Confirm", command= self.veri_confirm)
                 self.login_label.configure(text="Confirmation")
                 self.username_label.configure(text="Enter the code")
                 self.username_entry.place_forget()
                 self.username_entry.place(x=92*ratio, y = 370*ratio)
+                self.back_button.configure(command=self.switch_to_register)
+                self.back_button.place(x = 233*ratio, y = 788*ratio)
+
             else:
                 code = send_verification_code(email_input + "@gmail.com")
                 email = email_input
                 self.login_button.configure(text="Confirm", command= self.veri_confirm)
-                self.login_label.configure(text="Confirmation code")
+                clear_entry(self.username_entry)
+                self.login_label.configure(text="Confirmation")
                 self.username_label.configure(text="Enter the code")
                 self.username_entry.place_forget()
                 self.username_entry.place(x=92*ratio, y = 370*ratio)
+                self.back_button.configure(command=self.switch_to_register)
+                self.back_button.place(x = 233*ratio, y = 788*ratio)
         return code
     
     def veri_confirm(self):
         global code, email
-        self.login_label.configure(text="Confirmation code")
-        self.username_label.configure(text="Enter the code")
-        self.username_entry.place_forget()
-        self.username_entry.place(x=92*ratio, y = 370*ratio)
+
         veri_code = self.username_entry.get()
         if veri_code == '':
-            tkinter.messagebox.showinfo("Check")
+            tkinter.messagebox.showinfo("Check","Empty code. Try again.")
         else:
             if veri_code == code:
                 tkinter.messagebox.showinfo("Success!","Confirm success!")
@@ -191,21 +198,23 @@ class LoginMenu:
         clear_entry(self.username_entry)
         clear_entry(self.password_entry)
         clear_entry(self.confirm_password_entry)
+        self.login_label.configure(text="Create account")
         self.username_label.place_forget()
         self.username_label.configure(text="Username")
         self.password_entry.place_forget()
         self.password_entry.place_forget()
         self.login_button.place_forget()
+        self.back_button.place_forget()
         self.username_label.place(x=92*ratio, y=334*ratio)
         self.password_label.place(x=92*ratio, y=443*ratio)
         self.password_entry.place(x=92*ratio, y=478*ratio)
         self.account_label.configure(text="Already have account?")
         self.signup_button.configure(text="Sign in", command=self.switch_to_login)
+        self.email_register.place(x=159*ratio, y=758*ratio)
         # Tạo nhãn "Password" và đặt ở tọa độ (92,443)
         self.confirm_password_label.place(x=92*ratio, y=549*ratio)
         # Tạo hộp nhập liệu thứ nhất và đặt ở tọa độ (92,370)
         self.confirm_password_entry.place(x=92*ratio, y=584*ratio)
-
         self.login_button.configure(text="Register", command=self.register)
         self.login_button.place(x = 179*ratio, y = 651*ratio)
 
@@ -268,20 +277,33 @@ def check_gmail_in_string(s):
 def send_verification_code(receiver_email):
     # Tạo mã xác minh ngẫu nhiên
     global code
+    now = datetime.now()
+    now = now.replace(microsecond=0)
+    now = now.strftime("%d/%m%Y, %H:%M:%S")
     veri_code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
     code = veri_code
-    # Tạo nội dung email
-    subject = "Your verification code confirm the email to login/signup into Animal RaceTrack"
-    body = f"""Hello {receiver_email}.
-    We have sent a verificaion code to verify your login or registration.
-    Your verification code is: {veri_code}
-    You have sent this email because you requested the registration or login into your account.
-    If not, you can ignore this."""
-    message = f"Subject: {subject}\n\n{body}"
-
     # Thông tin tài khoản Gmail của bạn
     sender_email = "devteam.animalracetrack@gmail.com"
     password = "dzlh mowf obyr bfei"
+    # Tạo nội dung email
+    subject = "Your verification code confirm the email to login/signup into Animal RaceTrack"
+    body = f"""Hello {receiver_email}.
+    We have received requests to authenticate your email used to login or signup into our Animal RaceTrack game.
+    We have sent a verificaion code to verify your login or registration.
+    Your verification code is: {veri_code}
+
+    Please secure the encrypted OTP and follow the next steps to complete your login or registration to our Animal RaceTrack game.
+
+    You have sent this email because you requested the registration or login into your account.
+
+    If not, you can ignore this.
+
+    If you have any questions or other service requests, please contact us via:
+    Email: {sender_email}
+    """
+    message = f"Subject: {subject}\n\n{body}"
+
+
 
     # Gửi email
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
